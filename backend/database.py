b5787fa746c98951a98c6f4812eb91e20b24984e
Vinit Jadhav <vinit.jadhav.vj123@gmail.com>
2026-07-08 16:30:14 +0530

    ready to deploy

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,8 @@ def get_db_connection():
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT", "21442"))
         )
         return connection
     except Error as e:
